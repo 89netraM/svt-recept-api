82517093fb02b8ea1b5de59c8f3639263a631ce8
Mårten Åsberg <marten.asberg@outlook.com>
2020-06-27 21:30:16 +0200

    Image helper function

--- src/SVTRecipesController.ts
+++ src/SVTRecipesController.ts
@@ -7,6 +7,16 @@ import { CategoriesController, ChefController, FooterController, ProgramControll
 export class SVTRecipesController {
 	public static readonly defaultBaseURL: string = "https://program-recept-api.app.svt.se";
 
+	/**
+	 * Returns a URL to the given image.
+	 * @param imageId The id of the image.
+	 * @param format  The format of the image. `wide` is in 16:9 format.
+	 * @param width   The pixel with of the image. Max is 3000px.
+	 */
+	public static imageHelper(imageId: number, format: "wide" | "square" | "original" = "original", width: number | "unscaled" = 1280): string {
+		return `https://www.svtstatic.se/image/${format}/${width}/${imageId}`;
+	}
+
 	private readonly httpWrapper: IHttpWrapper;
 	private readonly baseURL: string;
 
